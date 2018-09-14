c162766556d62cfafe0fb82e4f0dc8f76bd30072
Dave Beck <dacb@uw.edu>
2018-09-14 12:20:17 -0700

    fleshing out tests for parser

--- barrelseq/test/test_command_line.py
+++ barrelseq/test/test_command_line.py
@@ -13,14 +13,14 @@ FASTA_PATH = os.path.join(os.path.dirname(__file__), 'test.fasta')
 BWA_PATH = os.path.join(os.path.dirname(__file__), 'bwa')
 BWA_MEM_OPTS = os.path.join(os.path.dirname(__file__), '-v')
 
+
 class TestCommandLine(unittest.TestCase):
     def setUp(self):
         self.parser = command_line.parser_create()
         fi, self.config_file = tempfile.mkstemp()
         os.fdopen(fi).close()
         self.project_dir = tempfile.mkdtemp()
-        args = self.parser.parse_args([
-                'config', 'create',
+        self.test_parse_list = [ 'config', 'create',
                 '--config-file', self.config_file,
                 '--project-dir', self.project_dir,
                 '--project-name', PROJECT_NAME,
@@ -29,16 +29,22 @@ class TestCommandLine(unittest.TestCase):
                 '--reference-fasta-path', FASTA_PATH,
                 '--bwa-path', BWA_PATH,
                 '--opts-bwa-mem', BWA_MEM_OPTS
-            ])
-        args.config_file.close()
+                ]
 
     def tearDown(self):
         shutil.rmtree(self.project_dir)
         os.remove(self.config_file)
 
     def test_parser_subcommand(self):
-        args = self.parser.parse_args(['config', 'edit', '--config-file', '/tmp/b', '--project-name', 'TestZ'])
+        args = self.parser.parse_args(self.test_parse_list)
         self.assertEqual(args.command, 'config')
+        self.assertEqual(args.config_command, 'create')
+        args.config_file.close()
+
+    def test_parser_default(self):
+        args = self.parser.parse_args(self.test_parse_list)
+        self.assertEqual(args.pair_ended, False)
+        self.assertEqual(args.samtools_path, 'samtools')
         args.config_file.close()
 
 
