e8ee3ed501207f5182c134e90d1434f8a923d824
Dave Beck <dacb@uw.edu>
2018-09-13 22:09:23 -0700

    building up parsers and subparsers

---
+++ barrelseq/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['command_line']
+
+__version__ = '0.1.0'
+
+SCRIPT_NAME = __name__
--- barrelseq/command_line.py
+++ barrelseq/command_line.py
@@ -64,15 +64,85 @@ def parser_create():
     """
     # create the top-level parser for sub-commands
     parser = argparse.ArgumentParser(prog=SCRIPT_NAME)
-    subparsers = parser.add_subparsers(help='command help')
+    subparsers = parser.add_subparsers(
+            title='{0} commands'.format(SCRIPT_NAME),
+            description='list of commands supported',
+            help='command help'
+            )
+    parser.add_argument('--version', action='version', 
+            version='{0} {1}'.format(SCRIPT_NAME, __version__)
+            )
     # config sub-command parser
     parser_config = subparsers.add_parser('config', help='config file utility help')
+
     # sample sub-command parser
     parser_sample = subparsers.add_parser('sample', help='sample management help')
+
     # engine sub-command parser
     parser_engine = subparsers.add_parser('engine', help='executation engine help')
+
+    # analysis sub-command parser
+    parser_analysis = subparsers.add_parser('analysis', help='data analysis help')
     # deseq2 sub-command parser
-    parser_deseq2 = subparsers.add_parser('deseq2', help='DESeq2 analysis help')
+    parser_deseq2 = parser_analysis.add_parser('deseq2', help='DESeq2 analysis help')
+    parser_deseq2.add_argument('--config-file', type=argparse.FileType('r'),
+            required=True, help='input configuration file'
+            )
+    parser_deseq2.add_argument('--output', type=str, required=True.
+            help='output file prefix')
+    parser_deseq2.add_argument('--group-A', type=str,
+            help='name of sample condition of first group')
+    parser_deseq2.add_argument('--group-B', type=str,
+            help='name of sample condition of second group')
+    parser_deseq2.add_argument('--regroup-A', type=str,
+            help='ad-hoc first group, e.g. EPS=uMax1,uMax2')
+    parser_deseq2.add_argument('--regroup-B', type=str,
+            help='ad-hoc second group, e.g. EPS=uMax1,uMax2')
+    parser_deseq2.add_argument('--shrinkage', 
+            choices=['normal', 'apeglm', 'ashr'],
+            default='apeglm',
+            help='method for shrinkage estimates, see DESeq2 docs'
+            )
+    parser_deseq2.add_argument('--transformation',
+            choices=['vst', 'rlog'],
+            default='vst'
+            help='variable stabilizing transformation or regularized log'
+            )
+    parser_deseq2.add_argument('--generate-figures',
+            help='should figures be generated'
+            )
+    # example sub-command parser as template for future analyses
+    parser_example = parser_analysis.add_parser('example', 
+            help='example analysis help')
+    parser_example.add_argument('--config-file', type=argparse.FileType('r'))
+    parser_example.add_argument('--integer-parameter', type=int,
+            help='example integer parameter for analysis'
+            )
+
     # extract sub-command parser
     parser_extract = subparsers.add_parser('extract', help='data extraction help')
+    parser_extract.add_argument('--config-file', type=argparse.FileType('r'),
+            required=True, help='input configuration file'
+            )
+    parser_extract.add_argument('--output', type=argparse.FileType('wb'),
+            required=True, help='output filename'
+            )
+    parser_extract.add_argument('--values',
+            choices=['TPM', 'raw', 'RPKM'],
+            default='TPM'
+            help='what values should be exported'
+            )
+    parser_extract.add_argument('--format',
+            choices=['tsv', 'csv', 'xls'],
+            default='tsv',
+            help='what file format should be used for the output'
+            )
+    parser_extract.add_argument('--samples',
+            nargs='*',
+            help='what samples should be included in the output'
+            )
+    parser_extract.add_argument()
+    parser_extract.add_argument()
+    parser_extract.add_argument()
+    parser_extract.add_argument()
     return parser
