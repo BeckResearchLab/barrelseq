9814668e29d8af2477cecb5cadfc5b2413b8ed50
Dave Beck <dacb@uw.edu>
2018-09-14 17:56:38 -0700

    working sample add and remove

--- barrelseq/sample.py
+++ barrelseq/sample.py
@@ -1,4 +1,5 @@
 import argparse
+import hashlib
 import os
 import sys
 
@@ -9,14 +10,15 @@ from barrelseq import config
 
 SAMPLE_INFO_FILE = 'sample_info.tsv'
 SAMPLE_INFO_COLUMNS = ['name', 'group', 'fastq_files', 'description',
-            'created', 'last_run', 'last_analyzed', 'md5sums'
+            'added', 'last_run', 'last_analyzed', 'md5sums'
         ]
+CHUNK_SIZE = 1024 * 1024 * 16
 
 
 def samples_init_df():
     """Creates a skeleton data frame with correct columns for sample info.
     """
-    return pd.DataFrame(index=['name'], columns=SAMPLE_INFO_COLUMNS)
+    return pd.DataFrame(columns=SAMPLE_INFO_COLUMNS)
 
 
 def load(cfg):
@@ -41,7 +43,7 @@ def load(cfg):
             attempts = attempts + 1
             if attempts > 1:
                 print('ERROR: unable to find the sample info file that '
-                    'should be in {0}'.format(filename))
+                    'should be in {}'.format(filename))
                 sys.exit(2)
             else:
                 save(cfg, samples_init_df())
@@ -58,9 +60,9 @@ def save(cfg, samples):
     """
     filename = os.path.join(cfg.project_dir, SAMPLE_INFO_FILE)
     try:
-        samples.to_csv(filename, sep='\t')
+        samples.to_csv(filename, sep='\t', index=False)
     except IOError as e:
-        print('ERROR: unable to create sample info file as {0}'.format(filename))
+        print('ERROR: unable to create sample info file as {}'.format(filename))
         sys.exit(2)
     return
 
@@ -68,10 +70,41 @@ def save(cfg, samples):
 def add(args):
     cfg = config.validate(args)
     samples = load(cfg)
+    print(samples)
+    errors = False
     # make sure the sample doesn't already exist
+    if args.name in samples.name.values:
+        print('ERROR: sample {} already exists'.format(args.name))
+        errors = True
     # make sure the fastq files are present
+    for filename in args.fastq_files:
+        if not os.path.exists(filename):
+            print('ERROR: fastq file {} does not exist'.format(filename))
+            errors = True
+    fastq_str = ','.join(args.fastq_files)
     # compute md5sums of the fastq files
-    # update created date
+    md5sums = []
+    if not errors:
+        for filename in args.fastq_files:
+            print('computing md5sum for file {}'.format(filename))
+            m = hashlib.md5()
+            with open(filename, 'rb') as f:
+                while True:
+                    chunk = f.read(CHUNK_SIZE)
+                    if chunk == b'':
+                        break
+                    m.update(chunk)
+            md5sums.append(m.hexdigest())
+        md5sums_str = ','.join(md5sums)
+        print(md5sums)
+        # update created date
+        added = pd.Timestamp.now()
+        samples.loc[len(samples)] = [ args.name, args.group, fastq_str,
+                args.description, added, None, None, md5sums_str
+            ]
+    if errors:
+        raise RuntimeError('at least one failure occured while '
+                'adding the sample')
     save(cfg, samples)
     return
 
@@ -79,6 +112,10 @@ def add(args):
 def remove(args):
     cfg = config.validate(args)
     samples = load(cfg)
-    print(samples)
+    if args.name in samples.name.values:
+        samples = samples[samples.name != args.name]
+    else:
+        raise RuntimeError('sample {} not found in sample info '
+                'file'.format(args.name))
     save(cfg, samples)
     return
