65478433747e4b2b21ea9231ec9c531fc2536d41
Dave Beck <dacb@uw.edu>
2018-09-14 18:24:43 -0700

    demonstration of how to get started building a subcommand

--- barrelseq/engine/run.py
+++ barrelseq/engine/run.py
@@ -1,6 +1,17 @@
 import argparse
 
+from barrelseq import config
+from barrelseq import sample
+
 
 def run(args):
-    print(args)
+    cfg = config.validate(args)
+    samples = sample.load(cfg)
+    # now the system is ready to go with a populated datafram with sample info
+    # and all of the system configuration data on the cfg object
+    # the prints below demonstrate what attributes are on cfg and what
+    # the schema of the sample_info table is
+    print(cfg)
+    print('\n\n')
+    print(samples)
     return
