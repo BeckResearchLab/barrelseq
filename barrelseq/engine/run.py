823a845d6928f4b36342f57f3a7f2c6ab3074147
Mitch Pesesky <mpesesky@uw.edu>
2018-09-19 15:07:43 -0700

    Finished command write-up

--- barrelseq/engine/run.py
+++ barrelseq/engine/run.py
@@ -8,15 +8,24 @@ from barrelseq import sample
 
 def make_bwa_cmd(sample_row, cfg):
     return "{} mem -M -t 1 {} {} > {}.sam".format(cfg.bwa_path, cfg.reference_fasta_path,
-                                                              sample_row['fastq_files'], sample_row.name)
+                                                  " ".join(sample_row['fastq_files'].split(",")), sample_row['name'])
 
 
 def make_view_cmd(name, cfg):
     return "{0} view -bt {1} -o {2}.bam {2}.sam".format(cfg.samtools_path, cfg.reference_fasta_path, name)
 
 
-def make_view_cmd(name, cfg):
+def make_sort_cmd(name, cfg):
     return "{0} sort {1}.bam {1}.sorted".format(cfg.samtools_path, name)
+
+
+def make_index_cmd(name, cfg):
+    return "{} index {}.sorted.bam".format(cfg.samtools_path, name)
+
+
+def make_htseq_cmd(name, cfg):
+    return "python {0} -f bam -m intersection-nonempty -s no -t gene -i ID " \
+           "{1}.sorted.bam {2} > {1}.summary.dat".format(cfg.htseq_count_path, name, cfg.reference_gff_path)
 
 
 def run(args):
@@ -29,17 +38,12 @@ def run(args):
 
     # If save_as_scripts is true, don't run anything, but put it all in a bash file
     # Update run date?
-    bwa_cmd = {}
-    view_command = {}
-    sort_command = {}
-    index_command = {}
-    htseq_command = {}
 
-    samples['bwa_cmd'] = samples.apply(lambda x: make_bwa_cmd(x, cfg))
+    samples['bwa_cmd'] = samples.apply(lambda x: make_bwa_cmd(x, cfg), axis=1)
     samples['view_cmd'] = samples['name'].map(lambda x: make_view_cmd(x, cfg))
     samples['sort_cmd'] = samples['name'].map(lambda x: make_sort_cmd(x, cfg))
-
-
+    samples['index_cmd'] = samples['name'].map(lambda x: make_index_cmd(x, cfg))
+    samples['htseq_cmd'] = samples['name'].map(lambda x: make_htseq_cmd(x, cfg))
 
     print(args)
     print('\n\n')
