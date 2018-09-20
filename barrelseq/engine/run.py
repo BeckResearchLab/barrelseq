5f6c0ee6c3c0b2279ddc1adc2086f4c18197e75d
Mitch Pesesky <mpesesky@uw.edu>
2018-09-20 10:42:24 -0700

    Finished command preparation

--- barrelseq/engine/run.py
+++ barrelseq/engine/run.py
@@ -1,31 +1,58 @@
 import argparse
 import multiprocessing as mp
-from subprocess import Popen
+import subprocess
 
 from barrelseq import config
 from barrelseq import sample
 
 
 def make_bwa_cmd(sample_row, cfg):
-    return "{} mem -M -t 1 {} {} > {}.sam".format(cfg.bwa_path, cfg.reference_fasta_path,
+    if cfg.opts_bwa_mem is not "null":
+        optional_args = cfg.opts_bwa_mem
+    else:
+        optional_args = ""
+    return "{} mem -M -t 1 {} {} {} > {}.sam".format(cfg.bwa_path, optional_args, cfg.reference_fasta_path,
                                                   " ".join(sample_row['fastq_files'].split(",")), sample_row['name'])
 
 
 def make_view_cmd(name, cfg):
-    return "{0} view -bt {1} -o {2}.bam {2}.sam".format(cfg.samtools_path, cfg.reference_fasta_path, name)
+    if cfg.opts_samtools_sam2bam is not "null":
+        optional_args = cfg.opts_bwa_mem
+    else:
+        optional_args = ""
+    return "{0} view {1} -bt {2} -o {3}.bam {3}.sam".format(cfg.samtools_path,
+                                                            optional_args, cfg.reference_fasta_path, name)
 
 
 def make_sort_cmd(name, cfg):
-    return "{0} sort {1}.bam {1}.sorted".format(cfg.samtools_path, name)
+    if cfg.opts_samtools_sort is not "null":
+        optional_args = cfg.opts_bwa_mem
+    else:
+        optional_args = ""
+    return "{0} sort {1} {2}.bam {2}.sorted".format(cfg.samtools_path, optional_args, name)
 
 
 def make_index_cmd(name, cfg):
-    return "{} index {}.sorted.bam".format(cfg.samtools_path, name)
+    if cfg.opts_index_mem is not "null":
+        optional_args = cfg.opts_bwa_mem
+    else:
+        optional_args = ""
+    return "{} index {} {}.sorted.bam".format(cfg.samtools_path, optional_args, name)
 
 
 def make_htseq_cmd(name, cfg):
-    return "python {0} -f bam -m intersection-nonempty -s no -t gene -i ID " \
-           "{1}.sorted.bam {2} > {1}.summary.dat".format(cfg.htseq_count_path, name, cfg.reference_gff_path)
+    if cfg.opts_htseq_count is not "null":
+        optional_args = cfg.opts_bwa_mem
+    else:
+        optional_args = ""
+    return "python {0} {1} -f bam -m intersection-nonempty -s no -t gene -i ID {2}.sorted.bam {3} > " \
+           "{2}.summary.dat".format(cfg.htseq_count_path, optional_args, name, cfg.reference_gff_path)
+
+
+def run_cmd(cmd):
+    err = subprocess.run(cmd, shell=True, stderr=subprocess.PIPE)
+    print(err)
+    return err
 
 
 def run(args):
@@ -38,12 +65,28 @@ def run(args):
 
     # If save_as_scripts is true, don't run anything, but put it all in a bash file
     # Update run date?
+    faidx = cfg.reference_fasta_path + ".fai"
+    try:
+        f = open(faidx, 'r')
+        f.close()
+    except FileNotFoundError:
+        faidx_cmd = "{} faidx {}".format(cfg.samtools_path, cfg.reference_fasta_path)
+        run_cmd(faidx_cmd)
+
 
     samples['bwa_cmd'] = samples.apply(lambda x: make_bwa_cmd(x, cfg), axis=1)
     samples['view_cmd'] = samples['name'].map(lambda x: make_view_cmd(x, cfg))
     samples['sort_cmd'] = samples['name'].map(lambda x: make_sort_cmd(x, cfg))
     samples['index_cmd'] = samples['name'].map(lambda x: make_index_cmd(x, cfg))
     samples['htseq_cmd'] = samples['name'].map(lambda x: make_htseq_cmd(x, cfg))
+
+    cmd_list = []
+    for step in ['bwa_cmd', 'view_cmd', 'sort_cmd', 'index_cmd', 'htseq_cmd']:
+        cmd_list.extend(samples[step].tolist())
+    print(cmd_list)
+
+    if args.save_as_scripts:
+        pass
 
     print(args)
     print('\n\n')
