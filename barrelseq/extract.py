4e09d2e9ab63d6db5ae06ab33cb0eac4ac6e2e8b
Dave Beck <dacb@uw.edu>
2019-07-17 15:23:45 -0700

    added skeleton of extractor

--- barrelseq/extract.py
+++ barrelseq/extract.py
@@ -1,6 +1,19 @@
 import argparse
+import os
+
+from barrelseq import config
+from barrelseq import sample
 
 
 def extract(args):
-    print(args)
+    cfg = config.validate(args)
+    samples = sample.validate(cfg)
+
+    os.chdir(os.path.join(cfg.project_dir, "workspace"))
+
+    if args.samples is not None:
+        samples = samples.loc[samples['name'].isin(args.samples)]
+
+    print(f'saving output to {args.output}')
+
     return
